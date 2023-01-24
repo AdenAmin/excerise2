974e3db5577ffa9c155653cecbcb6d5c92c75b37
yvngaden <adenamin52@gmail.com>
2023-01-24 14:11:24 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "wednesday", "thursday", "friday", "saturday"};
+        int[] highTemperatures = {45, 29, 10, 22, 41, 28, 33};
+        int[] precipitationProbabilities = {95, 60, 25, 5, 0, 75, 90};
+
+
+        for (int i = 0; i < highTemperatures.length; i++) {
+            if (precipitationProbabilities[i] >= 50 && highTemperatures[i] < 32) {
+                System.out.printf("It will snow on %s.%n", daysOfWeek[i]);
+            }
+        }
+    }
+}

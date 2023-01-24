af09a64e33343b0c211acdbb72f0ae1dad06ead0
yvngaden <adenamin52@gmail.com>
2023-01-24 14:17:51 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+//Aden Amin 01/24
+//CSCI 1660
+
 public class Main {
     public static void main(String[] args) {
         String[] daysOfWeek = {"Sunday", "Monday", "Tuesday", "wednesday", "thursday", "friday", "saturday"};
         int[] highTemperatures = {45, 29, 10, 22, 41, 28, 33};
         int[] precipitationProbabilities = {95, 60, 25, 5, 0, 75, 90};
-
 
         for (int i = 0; i < highTemperatures.length; i++) {
             if (precipitationProbabilities[i] >= 50 && highTemperatures[i] < 32) {
